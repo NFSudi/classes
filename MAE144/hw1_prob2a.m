e75a8cf086be0243ee6fea47884d52905b0aef99
NFSudi <nsudi@ucsd.edu>
2023-10-11 19:11:04 -0700

    changing hw1 prob2 adding personal polylogarithm

--- MAE144/hw1_prob2a.m
+++ MAE144/hw1_prob2a.m
@@ -1,5 +1,5 @@
-disp('Set up a new Diophantine problem')
-b=RR_poly([-2 2 -5 5],1), a=RR_poly([-1 1 -3 3 -6 6],1)
-f=RR_poly([-1 -1 -3 -3 -6 -6],1)
+clc; clear all; close all;
+disp('Problem 2a')
+b=RR_poly([-2 2 -5 5],1), a=RR_poly([-1 1 -3 3 -6 6],1), f=RR_poly([-1 -1 -3 -3 -6 -6],1)
 [x,y] = RR_diophantine(a,b,f), test=trim(a*x+b*y), residual1=norm(f-test)
-fprintf('Note that the solution {x,y} is proper (x.n=y.n), and solves a*x+b*y=f with ~ zero residual\n\n')
+fprintf('D=y/x is not proper and residual is close to 0\n') %y/x is not proper
--- MAE144/hw1_prob2b.m
+++ MAE144/hw1_prob2b.m
@@ -1,5 +1,6 @@
-disp('Set up a new Diophantine problem')
+clear; clc; close all;
+disp('Problem 2b')
 b=RR_poly([-2 2 -5 5],1), a=RR_poly([-1 1 -3 3 -6 6],1)
-f=RR_poly([-1 -1 -3 -3 -6 -6 -20],1)
+f=RR_poly([-1 -1 -3 -3 -6 -6 -20 -20 -20 -20 -20],1)
 [x,y] = RR_diophantine(a,b,f), test=trim(a*x+b*y), residual1=norm(f-test)
-fprintf('Note that the solution {x,y} is proper (x.n=y.n), and solves a*x+b*y=f with ~ zero residual\n\n')
+fprintf('The controller in 2a was improper. I added 5 roots (s=-20) to the target f. x increased its roots from 3 to 5. x and y now have 5 roots and make D proper\n')
